a449a2f328a8b50c7c9ae1f9773afac3d4b2721c
DaverArenas <daver.arenas@gmail.com>
2022-01-15 17:33:26 -0500

    Start of the project. Frontend with angular and initial commit

---
+++ angular/clientes-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Welcome to Best Bank';
+  mensaje: String = "We make your Dreams Come True";
+  Developer: String = "Develop by Daverson Arenas"
+
+
+
+}
