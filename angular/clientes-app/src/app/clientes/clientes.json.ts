118cf8ed5494fb73a04f5852621715549e6d31cd
DaverArenas <daver.arenas@gmail.com>
2022-01-16 11:26:28 -0500

    Creamos la lista de objetos del tipo cliente. archivo clientes.json.ts y la clase ClienteService para inyeccion de dependencias y comunicarnos con el Back

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  
+  {id: 1, nombre: 'Daverson', apellido: 'Arenas', email: 'daver.arenas@correo.com', createAt: '2021-01-11'},
+  {id: 2, nombre: 'Ana', apellido: 'Arenas', email: 'ana.arenas@correo.com', createAt: '2021-12-15'},
+  {id: 3, nombre: 'Olga', apellido: 'Gomez', email: 'olga.gomez@correo.com', createAt: '2022-01-15'},
+  {id: 4, nombre: 'James', apellido: 'Guzman', email: 'james.guzman@correo.com', createAt: '2020-08-25'},
+  {id: 5, nombre: 'Serena', apellido: 'Restrepo', email: 'serena.restrepo@correo.com', createAt: '2018-05-13'}
+];
