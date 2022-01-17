2b734d5dfdc7aba16c9f7fef76d39cec7922ffaf
DaverArenas <daver.arenas@gmail.com>
2022-01-16 22:42:04 -0500

    Creamos API Rest para compartir recursos entre el back y el front. Se crea el controlador @RestController y EndPoint para listar. Uso de Cors para compartir recursos. Implementamos servicio Angular con HttpClient

---
+++ eclipse/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/*Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andres', 'Guzman', 'profesor@sophos.com', '2012-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andrea', 'Guzman', 'profesor2@sophos.com', '2012-02-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Daniel', 'Gomez', 'profesor3@sophos.com', '2013-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Sofia', 'Arenas', 'profesor4@sophos.com', '2014-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Patricia', 'Perez', 'profesor5@sophos.com', '2015-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andrew', 'Mathew', 'profesor6@sophos.com', '2012-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Yeyo', 'Guzman', 'profesor7@sophos.com', '2012-12-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Pepo', 'Arenas', 'profesor8@sophos.com', '2012-11-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Manuel', 'Restrepo', 'profesor9@sophos.com', '2013-10-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Vanesa', 'Arenas', 'profesor10@sophos.com', '2014-09-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Paola', 'Perez', 'profesor11@sophos.com', '2015-08-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Skill', 'Guzman', 'profesor12@sophos.com', '2012-07-01');
